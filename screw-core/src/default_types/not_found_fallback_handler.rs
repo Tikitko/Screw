7d9054a8f98a58429b82a95979227c55b5fdc980
Nikita Bykov <bns.6587@gmail.com>
2022-01-04 11:47:30 +0300

    fix naming

--- screw-core/src/default_types/not_found_fallback_handler.rs
+++ screw-core/src/default_types/not_found_fallback_handler.rs
@@ -1,7 +1,7 @@
 use super::{Request, Response};
 use hyper::{Body, StatusCode};
 
-pub async fn default_fallback_handler(_request: Request) -> Response {
+pub async fn not_found_fallback_handler(_request: Request) -> Response {
     Response {
         http: hyper::Response::builder()
             .status(StatusCode::NOT_FOUND)
